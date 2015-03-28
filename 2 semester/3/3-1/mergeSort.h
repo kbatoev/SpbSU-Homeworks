ef11b6d8aadb2aa68a04c01f1c7ba234500b7f84
Konstantin Batoev <kbatoev@mail.ru>
2015-03-29 00:28:22 +0400

    Doxygen comments

--- 2 semester/3/3-1/mergeSort.h
+++ 2 semester/3/3-1/mergeSort.h
@@ -2,11 +2,25 @@
 
 #include "sorter.h"
 
+/**
+ * @brief The MergeSort class
+ * Class inherited of Sorter
+ */
+
 class MergeSort : Sorter
 {
 public:
+
+    /// constructor
     MergeSort();
+
+    /// realization of function in Sorter
     void sort(int *array, int sizeOfArray);
+
 private:
+
+    /// merge sort
+    /// @param left - first index of unsorted area
+    /// @param right - last index of unsorted area
     int* mergeSort(int* array, int first, int last);
 };
--- 2 semester/3/3-1/sorter.h
+++ 2 semester/3/3-1/sorter.h
@@ -1,9 +1,20 @@
 #pragma once
 
+/**
+ * @brief The Sorter class
+ * Interface of sortings classes
+ */
+
 class Sorter
 {
 public:
+
+    /// virtual destructor
     virtual ~Sorter() {}
+
+    /// virtual method sort
+    /// @param array - array that needs sorting
+    /// @param sizeOfArray - elements in array
     virtual void sort(int *array, int sizeOfArray) = 0;
 };
 
