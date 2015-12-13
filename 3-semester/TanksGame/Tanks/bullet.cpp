9ba3e82fbac1a9de9d61d8f72dec89c31321e3f3
kbatoev <kbatoev@mail.ru>
2015-12-13 13:20:23 +0300

    Code Refactoring in Bullet Class

--- 3-semester/TanksGame/Tanks/bullet.cpp
+++ 3-semester/TanksGame/Tanks/bullet.cpp
@@ -1,14 +1,17 @@
 #include "bullet.h"
 
 Bullet::Bullet(QPointF center, qreal angle, Landscape *landscape)
+    : bulletCenter(center),
+      initialCenter(center),
+      iteration(0),
+      bulletRadius(5),
+      coefficient(4.0),
+      angle(angle),
+      speed(55),
+      timer(nullptr),
+      landscape(landscape)
 {
-    this->bulletCenter = center;
-    this->radiusOfBurst = 40;
-    this->bulletRadius = 5;
-    this->coefficient = 4.0;
-    this->angle = angle;
-    this->speed = 55;
-    this->landscape = landscape;
+//    this->radiusOfBurst = 40;
 }
 
 void Bullet::drawBurst()
@@ -30,14 +33,11 @@ void Bullet::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QW
     pen.setWidth(2);
     painter->setPen(pen);
     painter->drawEllipse(bulletCenter, bulletRadius, bulletRadius);
-    //painter->drawRect(QRectF(bulletCenter.x() - bulletRadius, bulletCenter.y() - bulletRadius, 2 * bulletRadius, 2 * bulletRadius));
 }
 
 void Bullet::fly()
 {
-    initialCenter = bulletCenter;
     timer = new QTimer();
-    iteration = 0;
     connect(timer, SIGNAL(timeout()), this, SLOT(updatePosition()));
     timer->start(msec);
 }
@@ -45,7 +45,7 @@ void Bullet::fly()
 void Bullet::updatePosition()
 {
     iteration++;
-    qreal t = iteration / 10.0;
+    qreal t = iteration / periodOfSettingPositionOfBullet;
     qreal x = initialCenter.x() + speed * t * ::cos(-angle);
     qreal y = initialCenter.y() - speed * t * ::sin(-angle) + coefficient * t * t;
 
@@ -55,7 +55,7 @@ void Bullet::updatePosition()
     }
     else
     {
-        this->setVisible(false);
+        setVisible(false);
     }
     checkDistanceFromLandscape();
 
--- 3-semester/TanksGame/Tanks/constants.h
+++ 3-semester/TanksGame/Tanks/constants.h
@@ -4,3 +4,7 @@
 
 const int msec = 17;
 const qreal PI = 3.141592653;
+
+
+// for Bullet
+const qreal periodOfSettingPositionOfBullet = 10.0;
