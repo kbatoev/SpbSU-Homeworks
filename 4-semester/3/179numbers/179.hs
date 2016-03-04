621d86c830e4e42b8bacadab699e4a7e4b8b2220
kbatoev <kbatoev@mail.ru>
2016-03-04 17:03:24 +0300

    Added declaration of function

--- 4-semester/3/179numbers/179.hs
+++ 4-semester/3/179numbers/179.hs
@@ -10,7 +10,8 @@ list179 = list179
 	     	 list7 = [7 * 10 ^ (cntDigits x) + x| x <- list179]
 	     	 list9 = [9 * 10 ^ (cntDigits x) + x| x <- list179]
 	     	 list179 = [1, 7, 9] ++ concatBySorting list1 list7 list9
-
+		
+		 concatBySorting :: (Ord a) => [a] -> [a] -> [a] -> [a]
 	     	 concatBySorting (x:xs) (y:ys) (z:zs)
 				| x < y && x < z = [x] ++ concatBySorting xs (y:ys) (z:zs)
 				| y < x && y < z = [y] ++ concatBySorting (x:xs) ys (z:zs)
