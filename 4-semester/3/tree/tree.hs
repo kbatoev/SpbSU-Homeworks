647309a9120182f5021b073a203226648fe50d27
kbatoev <kbatoev@mail.ru>
2016-03-18 10:15:58 +0300

    Removed unnecessary Constructor : Leaf a, because it's made by (Node Nil a Nil)

--- 4-semester/3/tree/tree.hs
+++ 4-semester/3/tree/tree.hs
@@ -1,18 +1,15 @@
-data Tree a = Nil | Leaf a | Branch (Tree a) a (Tree a)
-			deriving (Show)
+data Tree a = Nil | Node (Tree a) a (Tree a) deriving (Show)
 
 countHeight :: Tree a -> Int
 countHeight Nil = 0
-countHeight (Leaf a) = 1
-countHeight (Branch leftTree a rightTree) = 1 + max (countHeight leftTree) (countHeight rightTree)
+countHeight (Node leftTree a rightTree) = 1 + max (countHeight leftTree) (countHeight rightTree)
 
 countMinDistance :: Tree a -> Int
 countMinDistance Nil = 0
-countMinDistance (Leaf a) = 1
-countMinDistance (Branch leftTree a rightTree) 
-												| countMinDistance leftTree == 0  = 1 + countMinDistance rightTree
-												| countMinDistance rightTree == 0 = 1 + countMinDistance leftTree
-												| otherwise                       = 1 + min (countMinDistance leftTree) (countMinDistance rightTree)
+countMinDistance (Node leftTree a rightTree) 
+					| countMinDistance leftTree == 0  = 1 + countMinDistance rightTree
+					| countMinDistance rightTree == 0 = 1 + countMinDistance leftTree
+					| otherwise                       = 1 + min (countMinDistance leftTree) (countMinDistance rightTree)
 
 --myTree :: Tree Int
---myTree = Branch (Branch (Leaf 12) 15 Nil) 20 (Branch Nil 21 (Branch Nil 22 (Branch Nil 23 (Leaf 24))))
+--myTree = Node (Node (Node Nil 12 Nil) 15 Nil) 20 (Node Nil 21 (Node Nil 22 (Node Nil 23 (Node Nil 24 Nil))))
