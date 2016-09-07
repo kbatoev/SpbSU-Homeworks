8b454eb0bbbd2933b56e30ad1c85a8b1eab5e2ef
kbatoev <kbatoev@mail.ru>
2016-09-07 13:22:10 +0300

    Now Programm uses Monad.State

--- 4-semester/6/dijkstra/dijkstra.hs
+++ 4-semester/6/dijkstra/dijkstra.hs
@@ -1,134 +1,110 @@
-{-# LANGUAGE FlexibleInstances #-}
-
-import Control.Monad
+import Control.Monad.State
 import Data.List
-import Control.Monad.Writer
+import Data.Ord
 
+data Edge = Edge {
+	cameFrom :: Int,
+	cameTo :: Int,
+	weight :: Int
+}
 
-data Edge = Edge {from :: Int, to :: Int, weight :: Int} deriving (Show)
-data Vertex = Vertex {number :: Int, distance :: Int} deriving (Show)
+data Vertex = Vertex {
+	number :: Int,
+	distance :: Maybe Int,
+	path :: [Int]
+}
+
+data Graph = Graph [Vertex] [Edge]
+
+instance Show Graph where
+	show (Graph vs es) = show vs
 
 instance Eq Vertex where
-	v1 == v2 = (number v1 == number v2)
+	v1 == v2 = (number v1) == (number v2)
 
-data Graph = Graph {verteces :: [Vertex], edges :: [Edge]} deriving (Show)
+instance Ord Vertex where
+	compare v1 v2 = compareDistances (distance v1) (distance v2)
 
-type PathToVertex = Writer [Int] Vertex
+compareDistances :: Maybe Int -> Maybe Int -> Ordering
+compareDistances Nothing x = GT
+compareDistances y Nothing = LT
+compareDistances (Just d1) (Just d2) = compare d1 d2
 
-getNumber :: PathToVertex -> Int
-getNumber = number . fst .runWriter
+instance Show Vertex where
+	show v = case distance v of
+			Just d  -> "Vertex: " ++ show (number v) ++ ". Distance: " ++ show d ++ "\n" ++ "Path: " ++ show ((number v): path v) ++ "\n"
+			Nothing -> "Vertex: " ++ show (number v) ++ " is unreacheable\n"
 
-getDistance :: PathToVertex -> Int
-getDistance = distance . fst . runWriter
+isNotGoingToDeletedVertex :: Int -> Edge -> Bool
+isNotGoingToDeletedVertex n edge = (cameTo edge) /= n
 
-getVertex :: PathToVertex -> Vertex
-getVertex = fst . runWriter 
+findBestVertex :: Graph -> (Maybe Vertex, Graph)
+findBestVertex g@(Graph [] _ ) = (Nothing, g)
+findBestVertex g@(Graph verteces es) = let (v:vs) = sort verteces
+					  	  			   in (Just v, (Graph vs (filter (isNotGoingToDeletedVertex (number v)) es))) 
 
-v1 = Vertex 1 100
-v2 = Vertex 2 50
+createVertex :: Vertex -> Edge -> Vertex
+createVertex (Vertex n Nothing p) (Edge f t w)  = (Vertex t Nothing [])
+createVertex (Vertex n (Just d) p) (Edge f t w) = Vertex t (Just (d + w)) (n:p)
 
-instance Eq PathToVertex where
-	vp1 == vp2 = fst (runWriter vp1) == fst (runWriter vp2)
+update :: [Edge] -> Maybe Vertex -> [Vertex]
+update [] _		   = []
+update es Nothing  = []
+update (e:es) v1@(Just v) = if (cameFrom e == number v)
+						 then (createVertex v e) : update es v1
+						 else update es v1
+
+chooseNewVerteces :: [Vertex] -> [Vertex] -> [Vertex]
+chooseNewVerteces nvs []         = nvs
+chooseNewVerteces [] oldvs       = oldvs
+chooseNewVerteces (nv:nvs) oldvs = (chooseVertex nv (filter (==nv) oldvs)) : chooseNewVerteces nvs (filter (/=nv) oldvs)
+
+chooseVertex :: Vertex -> [Vertex] -> Vertex
+chooseVertex nv [] = nv
+chooseVertex nv (v:vs) = if compare nv v == LT
+		  				 then chooseVertex nv vs
+		  				 else chooseVertex v vs
+
+updateLength :: Maybe Vertex -> State Graph (Maybe Vertex)
+updateLength Nothing = return Nothing
+updateLength v1@(Just v) = state $ \(Graph vs es) -> (v1, (Graph (chooseNewVerteces (update (filter (isFrom (number v)) es) v1) vs) es)) 
+
+isFrom :: Int -> Edge -> Bool
+isFrom n e = n == cameFrom e
+
+findBestVertexState :: State Graph (Maybe Vertex)
+findBestVertexState = state findBestVertex
+
+oneStepDijkstra :: State Graph (Maybe Vertex)
+oneStepDijkstra = findBestVertexState >>= updateLength
+
+dijkstra :: State Graph [Vertex]
+dijkstra = do
+			currentVertex <- oneStepDijkstra
+			case currentVertex of
+				Just v   -> do
+							vs <- dijkstra
+							return (v:vs)
+				Nothing  -> return []
 
 
-instance Ord PathToVertex where
-	compare v1 v2
-				| (distance $ getVertex v1) > (distance $ getVertex v2) = GT
-				| getVertex v1 == getVertex v2        					= EQ
-				| otherwise                     						= LT 
+
+v1 = Vertex 1 (Just 0) []
+vs = [v1, (Vertex 2 Nothing []), (Vertex 3 Nothing [])]
+es = [(Edge 1 2 10), (Edge 1 3 4), (Edge 2 3 5)]
+g1 = Graph vs es
+
+g2 = Graph [v1, (Vertex 2 Nothing []), (Vertex 3 Nothing [])] [(Edge 1 2 10)]
+g3 = Graph [(Vertex 1 Nothing []), (Vertex 2 Nothing []), (Vertex 3 (Just 0) [])] [(Edge 1 2 10)]
 
 
-addEdge :: PathToVertex -> Int -> Int -> PathToVertex
-addEdge vp l newDistance = do
-				let (v, edges) = runWriter vp
-				let n = getNumber vp
-				tell (l:edges) 
-				return (Vertex n newDistance)
+testEdges = [(Edge 1 2 100), (Edge 1 3 150), (Edge 1 4 70), (Edge 2 3 20), (Edge 2 5 30), (Edge 3 5 1), (Edge 4 3 200), (Edge 4 5 1000)]
+testVerteces = [(Vertex 1 (Just 0) []), (Vertex 2 Nothing []), (Vertex 3 Nothing []), (Vertex 4 Nothing []), (Vertex 5 Nothing [])]
+testGraph = Graph testVerteces testEdges
 
-addToPath :: PathToVertex -> Vertex -> PathToVertex 
-addToPath fromPath to@(Vertex n2 d2) = fromPath >> tell [n2] >> return to
+{--
 
-
-makePathVertex :: Int -> Vertex -> PathToVertex
-makePathVertex edgeLength v = tell [number v] >> return (Vertex (number v) (edgeLength + distance v))
-
-
---xxx = (addToPath (makePathVertex v1) v2)
-
---data MyGraph = MyGraph {vertecess :: [Vertex], edgess :: [Edge]}
-
-testGraph = Graph [( Vertex 1 0), (Vertex 2 0), (Vertex 3 0), (Vertex 4 0), (Vertex 5 0)] [(Edge 1 2 100), (Edge 1 3 150), (Edge 1 4 70), (Edge 2 3 20), (Edge 2 5 30), (Edge 3 5 1), (Edge 4 3 200), (Edge 4 5 1000)]
-graph1 = Graph [(Vertex 1 (0)), (Vertex 2 (-1)), (Vertex 3 (-1))] [(Edge 1 2 10), (Edge 1 3 20), (Edge 2 3 100)]
-
-{--markVerteces :: Graph -> Int -> Graph
-markVerteces (Graph vl el) startVertex = Graph (markVerteces' vl startVertex) el
-	where 
-		markVerteces' :: [(Int, v)] -> Int -> [(Int, Int)]
-		markVerteces' [] _ = []
-		markVerteces' ((n, _):vs) startVertex
-						      | n == startVertex = (n, 0) : (markVerteces' vs startVertex)
-						      | otherwise        = (n, -1) : (markVerteces' vs startVertex)
+To run dijkstra, use this command:
+fst $ (runState dijkstra ) testGraph 
 
 --}
-
-makeWriter :: Writer [String] (Int, Int) -> Int -> Int -> Writer [String] (Int, Int)
-makeWriter w number distance = w >> tell ["We came to vertex: " ++ show number] >> return (number, distance)
-
-createAll :: [Vertex] -> [PathToVertex]
-createAll []     = []
-createAll (v:vs) = (return v) : (createAll vs)
-
-dijkstra :: Graph -> Int -> [PathToVertex]
-dijkstra g n = dijkstra' g n (createAll $ verteces g) 0
-	where 
-		dijkstra' :: Graph -> Int -> [PathToVertex] -> Int -> [PathToVertex]
-		dijkstra' g@(Graph vs es) n ps currentLength = do
-					--tell ["Here's some testing"]
-					let edges = filter (\edge -> (from edge) == n) es
-					let newPs = updateLength edges ps currentLength
-					--addLog :: Writer [String] (Int, Int)
-					--addLog = tell ["We visited vertex: " ++ show number]
-					--let newLog = makeWriter w n currentLength
-					
-			
-					case (findNextVertex newPs currentLength) of
-						Nothing -> newPs
-						Just vp -> dijkstra' g (getNumber vp) newPs (getDistance vp)
-
-
-		findPosition :: [PathToVertex] -> Int -> Int -> Int
-		findPosition (p:ps) aim position
-						| getNumber p == aim = position
-						| otherwise   = findPosition ps aim (position + 1)
-
-		updateLength :: [Edge] -> [PathToVertex] -> Int -> [PathToVertex]
-		updateLength [] ps _ = ps
-		updateLength (edge:es) ps currentMin = do
-								let positionInList = findPosition ps (to edge) 0
-								let finishVertex = ps !! positionInList
-								let currentLength = getDistance finishVertex
-
-								case (currentLength == -1 || currentLength > currentMin + (weight edge)) of
-									True  -> updateLength es (setLengthToVertex ps (from edge) (to edge) (currentMin + (weight edge))) currentMin
-									False -> updateLength es ps currentMin
-
-									where setLengthToVertex :: [PathToVertex] -> Int -> Int -> Int -> [PathToVertex]
-									      setLengthToVertex [] _ _ _ = []
-									      setLengthToVertex (p:ps) fromNumber currentNumber currentLength =
-															  if getNumber p == currentNumber
-														      then (addEdge p fromNumber currentLength): ps
-														      else p : (setLengthToVertex ps fromNumber currentNumber currentLength)
-
-		
-		sortRestVerteces :: [PathToVertex] -> Int -> [PathToVertex]
-		sortRestVerteces ps length = sort (filter (\x -> getDistance x > length) ps)	
-		
-		findNextVertex :: [PathToVertex] -> Int -> Maybe PathToVertex
-		findNextVertex [] _ = Nothing
-		findNextVertex ps currentLength = do
-								let newPs = sortRestVerteces ps currentLength				
-								if length newPs > 0
-								then Just (head ps)
-								else Nothing
-
-
