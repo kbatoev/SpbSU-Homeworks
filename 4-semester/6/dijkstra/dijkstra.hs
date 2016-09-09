6da97a060ecee2087ec8ead686b5ffb368784516
kbatoev <kbatoev@mail.ru>
2016-09-09 16:20:08 +0300

    Real using of State

--- 4-semester/6/dijkstra/dijkstra.hs
+++ 4-semester/6/dijkstra/dijkstra.hs
@@ -14,8 +14,10 @@ data Vertex = Vertex {
 	path :: [Int]
 }
 
-data Graph = Graph [Vertex] [Edge]
-
+data Graph = Graph {
+	vertecess :: [Vertex],
+	edgess :: [Edge]
+}
 instance Show Graph where
 	show (Graph vs es) = show vs
 
@@ -24,35 +26,24 @@ instance Eq Vertex where
 
 instance Ord Vertex where
 	compare v1 v2 = compareDistances (distance v1) (distance v2)
-
-compareDistances :: Maybe Int -> Maybe Int -> Ordering
-compareDistances Nothing x = GT
-compareDistances y Nothing = LT
-compareDistances (Just d1) (Just d2) = compare d1 d2
+					where
+						compareDistances :: Maybe Int -> Maybe Int -> Ordering
+						compareDistances Nothing x = GT
+						compareDistances y Nothing = LT
+						compareDistances (Just d1) (Just d2) = compare d1 d2
 
 instance Show Vertex where
 	show v = case distance v of
 			Just d  -> "Vertex: " ++ show (number v) ++ ". Distance: " ++ show d ++ "\n" ++ "Path: " ++ show ((number v): path v) ++ "\n"
 			Nothing -> "Vertex: " ++ show (number v) ++ " is unreacheable\n"
 
-isNotGoingToDeletedVertex :: Int -> Edge -> Bool
-isNotGoingToDeletedVertex n edge = (cameTo edge) /= n
-
-findBestVertex :: Graph -> (Maybe Vertex, Graph)
-findBestVertex g@(Graph [] _ ) = (Nothing, g)
-findBestVertex g@(Graph verteces es) = let (v:vs) = sort verteces
-					  	  			   in (Just v, (Graph vs (filter (isNotGoingToDeletedVertex (number v)) es))) 
-
 createVertex :: Vertex -> Edge -> Vertex
 createVertex (Vertex n Nothing p) (Edge f t w)  = (Vertex t Nothing [])
 createVertex (Vertex n (Just d) p) (Edge f t w) = Vertex t (Just (d + w)) (n:p)
 
-update :: [Edge] -> Maybe Vertex -> [Vertex]
-update [] _		   = []
-update es Nothing  = []
-update (e:es) v1@(Just v) = if (cameFrom e == number v)
-						 then (createVertex v e) : update es v1
-						 else update es v1
+update :: [Edge] -> Vertex -> [Vertex]
+update [] _		= []
+update (e:es) v = (createVertex v e) : update es v
 
 chooseNewVerteces :: [Vertex] -> [Vertex] -> [Vertex]
 chooseNewVerteces nvs []         = nvs
@@ -65,24 +56,31 @@ chooseVertex nv (v:vs) = if compare nv v == LT
 		  				 then chooseVertex nv vs
 		  				 else chooseVertex v vs
 
-updateLength :: Maybe Vertex -> State Graph (Maybe Vertex)
-updateLength Nothing = return Nothing
-updateLength v1@(Just v) = state $ \(Graph vs es) -> (v1, (Graph (chooseNewVerteces (update (filter (isFrom (number v)) es) v1) vs) es)) 
+updateLength :: Vertex -> State Graph ()
+updateLength v = do
+				 g <- get
+				 let edges = edgess g
+				 let possibleVs = update (filter (\edge -> cameFrom edge == number v) edges) v
+				 let newVs = chooseNewVerteces possibleVs (vertecess g)
+				 put (Graph newVs edges)
 
-isFrom :: Int -> Edge -> Bool
-isFrom n e = n == cameFrom e
-
-findBestVertexState :: State Graph (Maybe Vertex)
-findBestVertexState = state findBestVertex
-
-oneStepDijkstra :: State Graph (Maybe Vertex)
-oneStepDijkstra = findBestVertexState >>= updateLength
+findBestVertex :: State Graph (Maybe Vertex)
+findBestVertex = do
+				g <- get
+				case (vertecess g) of
+					[] -> return $ Nothing
+					vs -> do
+						    let (v:restVs) = sort vs
+						    let restEs = filter (\edge -> cameTo edge /= number v) (edgess g)
+						    put (Graph restVs restEs)
+						    return (Just v)
 
 dijkstra :: State Graph [Vertex]
 dijkstra = do
-			currentVertex <- oneStepDijkstra
+			currentVertex <- findBestVertex
 			case currentVertex of
 				Just v   -> do
+							updateLength v
 							vs <- dijkstra
 							return (v:vs)
 				Nothing  -> return []
@@ -96,7 +94,6 @@ g1 = Graph vs es
 
 g2 = Graph [v1, (Vertex 2 Nothing []), (Vertex 3 Nothing [])] [(Edge 1 2 10)]
 g3 = Graph [(Vertex 1 Nothing []), (Vertex 2 Nothing []), (Vertex 3 (Just 0) [])] [(Edge 1 2 10)]
-
 
 testEdges = [(Edge 1 2 100), (Edge 1 3 150), (Edge 1 4 70), (Edge 2 3 20), (Edge 2 5 30), (Edge 3 5 1), (Edge 4 3 200), (Edge 4 5 1000)]
 testVerteces = [(Vertex 1 (Just 0) []), (Vertex 2 Nothing []), (Vertex 3 Nothing []), (Vertex 4 Nothing []), (Vertex 5 Nothing [])]
