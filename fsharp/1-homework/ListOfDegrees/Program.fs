5420181226ab6294d5ae69563f111c67fc1d5517
Konstantin Batoev <kbatoev@mail.ru>
2017-03-07 20:03:39 +0400

    Homework 1.4

---
+++ fsharp/1-homework/ListOfDegrees/Program.fs
@@ -0,0 +1,8 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.net
+
+let listOfDegrees n m =
+    let degreeNM =
+        let rec n2 i acc = if i = n + m then acc else n2 (i + 1) (acc * 2)
+        n2 0 1
+    let rec helper list i degree = if i < n then list else helper (degree :: list) (i - 1) (degree / 2)
+    helper [] (n + m) degreeNM
