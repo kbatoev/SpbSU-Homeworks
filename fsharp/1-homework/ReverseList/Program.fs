6a72f2486cfd6fe0665445dd49a9f34eb6f802ae
Konstantin Batoev <kbatoev@mail.ru>
2017-03-07 19:28:13 +0400

    Homework 1.3

---
+++ fsharp/1-homework/ReverseList/Program.fs
@@ -0,0 +1,7 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.net
+let reverseList ls = 
+    let rec helper list acc =
+        match list with
+        | [] -> acc
+        | (x :: xs) -> helper xs (x :: acc)
+    helper ls []
