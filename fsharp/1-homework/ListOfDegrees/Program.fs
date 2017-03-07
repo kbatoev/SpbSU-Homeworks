f9291d4a4838893c86fb4687f872fd138aa28911
Konstantin Batoev <kbatoev@mail.ru>
2017-03-07 20:09:16 +0400

    Some refactoring

--- fsharp/1-homework/ListOfDegrees/Program.fs
+++ fsharp/1-homework/ListOfDegrees/Program.fs
@@ -2,7 +2,7 @@
 
 let listOfDegrees n m =
     let degreeNM =
-        let rec n2 i acc = if i = n + m then acc else n2 (i + 1) (acc * 2)
-        n2 0 1
+        let rec toDegree i acc = if i = n + m then acc else toDegree (i + 1) (acc * 2)
+        toDegree 0 1
     let rec helper list i degree = if i < n then list else helper (degree :: list) (i - 1) (degree / 2)
     helper [] (n + m) degreeNM
