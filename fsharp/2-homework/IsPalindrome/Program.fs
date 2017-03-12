83f686133eb22183f2467124d850f9a2e1e06652
Konstantin Batoev <kbatoev@mail.ru>
2017-03-12 18:56:53 +0400

    Homework 2. Task 3

---
+++ fsharp/2-homework/IsPalindrome/Program.fs
@@ -0,0 +1,15 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.net
+
+
+module Microsoft.FSharp.Collections.List
+
+let isPalindrome str =
+    let listOfChars = 
+        str |> Seq.toList
+    let reverseList = 
+        let rec reverse list acc = 
+            match list with
+            | [] -> acc
+            | x :: xs -> reverse xs (x :: acc)
+        reverse listOfChars []
+    listOfChars |> List.zip reverseList |> List.fold (fun acc (x,y) -> (x = y) && acc) true
