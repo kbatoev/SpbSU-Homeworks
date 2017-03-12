bf9d16850a22eb3979dfb5dbf34ee6528e5f8cd6
Konstantin Batoev <kbatoev@mail.ru>
2017-03-12 15:54:14 +0400

    Homework 2. Task 2

---
+++ fsharp/2-homework/FirstPosition/Program.fs
@@ -0,0 +1,8 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.net
+
+let findFirstPosition list number =
+    let rec helper newList position =
+        match newList with
+        | [] -> -1
+        | x :: xs -> if x = number then position else helper xs (position + 1)
+    helper list 0
