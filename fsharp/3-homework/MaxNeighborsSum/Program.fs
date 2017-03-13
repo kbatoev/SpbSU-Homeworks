945a31e90afcf68c25046f7c03fb29f18b824732
Konstantin Batoev <kbatoev@mail.ru>
2017-03-13 13:38:52 +0400

    Homework 3. Task 2

---
+++ fsharp/3-homework/MaxNeighborsSum/Program.fs
@@ -0,0 +1,13 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.net
+
+let maxNeighborsSum list =
+    let rec helper a b c list maxSum currentPosition maxPosition =
+        let currentSum = a + c
+        match list with
+        | [] -> if currentSum >= maxSum then currentPosition else maxPosition
+        | x :: xs -> if currentSum >= maxSum
+                     then helper b c x xs currentSum (currentPosition + 1) currentPosition
+                     else helper b c x xs maxSum (currentPosition + 1) maxPosition
+    match list with
+    | x :: y :: z :: xs -> Some (helper x y z xs (x + z) 1 1)
+    | _ -> None 
