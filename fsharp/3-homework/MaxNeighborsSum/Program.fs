6e4f6049d97562843399449a4e3ea5d11452db8d
Konstantin Batoev <kbatoev@mail.ru>
2017-03-17 07:05:00 +0400

    Now neighbors are two consequent elements

--- fsharp/3-homework/MaxNeighborsSum/Program.fs
+++ fsharp/3-homework/MaxNeighborsSum/Program.fs
@@ -1,13 +1,12 @@
 ﻿// Дополнительные сведения о F# см. на http://fsharp.net
 
 let maxNeighborsSum list =
-    let rec helper a b c list maxSum currentPosition maxPosition =
-        let currentSum = a + c
-        match list with
-        | [] -> if currentSum >= maxSum then currentPosition else maxPosition
-        | x :: xs -> if currentSum >= maxSum
-                     then helper b c x xs currentSum (currentPosition + 1) currentPosition
-                     else helper b c x xs maxSum (currentPosition + 1) maxPosition
+    let rec helper currentList maxSum currentPosition maxPosition =
+        match currentList with
+        | [x] -> maxPosition
+        | x :: y :: xs -> if x + y > maxSum
+                          then helper (y :: xs) (x + y) (currentPosition + 1) currentPosition
+                          else helper (y :: xs) maxSum (currentPosition + 1) maxPosition
     match list with
-    | x :: y :: z :: xs -> Some (helper x y z xs (x + z) 1 1)
+    | x :: y :: xs -> Some (helper (y :: xs) (x + y) 2 1)
     | _ -> None 
