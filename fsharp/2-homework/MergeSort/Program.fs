a7d13e64d92a9d2f2bce2f847f4f603563649fe2
Konstantin Batoev <kbatoev@mail.ru>
2017-03-17 10:30:49 +0400

    Tried to deal with StackOverflowException

--- fsharp/2-homework/MergeSort/Program.fs
+++ fsharp/2-homework/MergeSort/Program.fs
@@ -1,22 +1,30 @@
 ﻿// Дополнительные сведения о F# см. на http://fsharp.net
 
-module Microsoft.FSharp.Collections.List
+//module Microsoft.FSharp.Collections.List
+//module System.IO.StreamWriter
 
-let rec mergeSort list =
-    let cnt = List.length list / 2
-
-    let rec merge l1 l2 = 
-        match l1 with
-        | [] -> l2
-        | (x :: xs) -> match l2 with
-                        | [] -> l1
-                        | (y :: ys) ->  if x < y 
-                                        then x :: (merge xs l2) 
-                                        else y :: (merge l1 ys)
+let rec mergeSort list =    
     match list with
     | [x] -> [x]
-    | _   -> merge (list |> Seq.take cnt |> Seq.toList |> mergeSort) (list |> Seq.skip cnt |> Seq.toList |> mergeSort)
+    | _   -> let cnt = List.length list / 2
 
+             let merge list1 list2 =
+                let rec helper acc (l1, l2) =
+                    match (l1, l2) with
+                    | ([], []) -> List.rev acc
+                    | ([], y :: ys) -> helper (y :: acc) ([], ys)
+                    | (x :: xs, []) -> helper (x :: acc) (xs, [])
+                    | (x :: xs, y :: ys) when x < y -> helper (x :: acc) (xs, l2)
+                    | (x :: xs, y :: ys) -> helper (y :: acc) (l1, ys) 
+                helper [] (list1, list2)
+    
+             let l1, l2 =
+                let rec splitList currentList acc n = if n = cnt
+                                                      then currentList, acc
+                                                      else splitList (List.tail currentList) ((List.head currentList) :: acc) (n + 1)
+                splitList list [] 0
+             merge (mergeSort l1) (mergeSort l2)
 
-let list = [1; 6; 2; 0; -1; 4; 3]
-list |> mergeSort |> printf "The sorted list is %A\n"
+// let list = [1; 6; 2; 0; -1; 4; 3]
+let list = List.init 20000 id
+mergeSort list |> printf "The sorted list is %A\n"
