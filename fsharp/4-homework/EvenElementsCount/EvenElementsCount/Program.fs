6dabd3d129e51b115f7ed61a7888feabde3649f6
Konstantin Batoev <kbatoev@mail.ru>
2017-03-26 18:52:15 +0400

    Added test project

--- fsharp/4-homework/EvenElementsCount/EvenElementsCount/Program.fs
+++ fsharp/4-homework/EvenElementsCount/EvenElementsCount/Program.fs
@@ -1,39 +1,12 @@
 ﻿// Дополнительные сведения о F# см. на http://fsharp.org
 // Дополнительную справку см. в проекте "Учебник по F#".
 
+namespace EvenElements
 
-open NUnit.Framework
-open FsUnit
+module Functions = 
 
-let list1 = [1; 3; 5; 7]
-let list2 = [2; 4; 6; 8]
-let list3 = []
-let list4 = [2]
-let list5 = [3]
+  let countEvenElementsFilter xs = (List.filter (fun x -> x % 2 = 0) xs) |> List.length
 
+  let countEvenElementsFold xs = List.fold (fun acc x -> if x % 2 = 0 then acc + 1 else acc) 0 xs
 
-
-let countEvenElementsFilter = List.filter (fun x -> x % 2 = 0) >> List.length
-
-let countEvenElementsFold = List.fold (fun acc x -> if x % 2 = 0 then acc + 1 else acc) 0
-
-let countEvenElementsMap = List.map (fun x -> (x  + 1 ) % 2) >> List.sum
-
-
-countEvenElementsFilter list1 |> should equal 0
-countEvenElementsFilter list2 |> should equal 4
-countEvenElementsFilter list3 |> should equal 0
-countEvenElementsFilter list4 |> should equal 1
-countEvenElementsFilter list5 |> should equal 0
-
-countEvenElementsFold list1 |> should equal 0
-countEvenElementsFold list2 |> should equal 4
-countEvenElementsFold list3 |> should equal 0
-countEvenElementsFold list4 |> should equal 1
-countEvenElementsFold list5 |> should equal 0
-
-countEvenElementsMap list1 |> should equal 0
-countEvenElementsMap list2 |> should equal 4
-countEvenElementsMap list3 |> should equal 0
-countEvenElementsMap list4 |> should equal 1
-countEvenElementsMap list5 |> should equal 0
+  let countEvenElementsMap xs = List.map (fun x -> (x + 1) % 2) xs |> List.sum
---
+++ fsharp/4-homework/EvenElementsCount/EvenElementsCountTests/Library1.fs
@@ -0,0 +1,77 @@
+﻿namespace EvenElementsCountTests
+
+open EvenElements.Functions
+open NUnit.Framework
+open FsUnit
+
+module TestModule = 
+
+  let list1 = [1; 3; 5; 7]
+  let list2 = [2; 4; 6; 8]
+  let list3 = []
+  let list4 = [2]
+  let list5 = [3]
+  
+
+  [<Test>]
+  let checkListWithOddElements_By_Filter () = 
+    countEvenElementsFilter list1 |> should equal 0
+  
+  [<Test>]
+  let checkListWithEvenElements_By_Filter () = 
+    countEvenElementsFilter list2 |> should equal 4
+
+  [<Test>]
+  let checkEmptyList_By_Filter () = 
+    countEvenElementsFilter list3 |> should equal 0
+
+  [<Test>]
+  let checkListWithEvenElement_By_Filter () = 
+    countEvenElementsFilter list4 |> should equal 1
+
+  [<Test>]
+  let checkListWithOddElement_By_Filter () = 
+    countEvenElementsFilter list5 |> should equal 0
+
+  // For countEvenElementsMap
+  [<Test>]
+  let checkListWithOddElements_By_Map () = 
+    countEvenElementsMap list1 |> should equal 0
+  
+  [<Test>]
+  let checkListWithEvenElements_By_Map () = 
+    countEvenElementsMap list2 |> should equal 4
+
+  [<Test>]
+  let checkEmptyList_By_Map () = 
+    countEvenElementsMap list3 |> should equal 0
+
+  [<Test>]
+  let checkListWithEvenElement_By_Map () = 
+    countEvenElementsMap list4 |> should equal 1
+
+  [<Test>]
+  let checkListWithOddElement_By_Map () = 
+    countEvenElementsMap list5 |> should equal 0
+
+
+  // For countEvenElementsFold
+  [<Test>]
+  let checkListWithOddElements_By_Fold () = 
+    countEvenElementsFold list1 |> should equal 0
+  
+  [<Test>]
+  let checkListWithEvenElements_By_Fold () = 
+    countEvenElementsFold list2 |> should equal 4
+
+  [<Test>]
+  let checkEmptyList_By_Fold () = 
+    countEvenElementsFold list3 |> should equal 0
+
+  [<Test>]
+  let checkListWithEvenElement_By_Fold () = 
+    countEvenElementsFold list4 |> should equal 1
+
+  [<Test>]
+  let checkListWithOddElement_By_Fold () = 
+    countEvenElementsFold list5 |> should equal 0
