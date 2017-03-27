fd6d818ad24bfa731d67ea77f13b1fb8efc2aabf
Konstantin Batoev <kbatoev@mail.ru>
2017-03-27 10:13:16 +0400

    Homework 4. Task 3

---
+++ fsharp/4-homework/ArithmeticExpression/ArithmeticExpression/Count.fs
@@ -0,0 +1,19 @@
+﻿namespace Expression
+
+open NUnit.Framework
+open FsUnit
+
+module Arithmetic = 
+
+  type ArithmeticExpression =
+    | Number of int
+    | Plus of ArithmeticExpression * ArithmeticExpression
+    | Minus of ArithmeticExpression * ArithmeticExpression
+    | Multiplication of ArithmeticExpression * ArithmeticExpression
+
+  let rec countArithmeticExpression expression = 
+    match expression with
+    | Number x -> x
+    | Plus (expr1, expr2) -> countArithmeticExpression expr1 + countArithmeticExpression expr2
+    | Minus (expr1, expr2) -> countArithmeticExpression expr1 - countArithmeticExpression expr2
+    | Multiplication (expr1, expr2) -> countArithmeticExpression expr1 * countArithmeticExpression expr2
---
+++ fsharp/4-homework/ArithmeticExpression/ArithmeticExpressionTest/Library1.fs
@@ -0,0 +1,43 @@
+﻿namespace ArithmeticExpressionTest
+
+open Expression.Arithmetic
+open NUnit.Framework
+open FsUnit
+
+
+module Tests =
+
+  let expressionNumber = Number 5
+  [<Test>]
+  let checkNumberExpression () =
+    countArithmeticExpression expressionNumber |> should equal 5
+
+
+  let expr_1_Plus_4 = Plus (Number 1, Number 4)
+  [<Test>]
+  let checkSumExpression () =
+    countArithmeticExpression expr_1_Plus_4 |> should equal 5
+
+
+  let expr_90_Minus_34 = Minus (Number 90, Number 34)
+  [<Test>]
+  let checkMinusExpression () =
+    countArithmeticExpression expr_90_Minus_34 |> should equal 56
+
+
+  let expr_23_Multiplication_71 = Multiplication (Number 23, Number 71)
+  [<Test>]
+  let checkMultiplicationExpression () =
+    countArithmeticExpression expr_23_Multiplication_71 |> should equal 1633
+
+
+  let bigExpression = Multiplication (Plus (Number 45, Number 78), Minus (Number 123, Number 90))
+  [<Test>]
+  let checkBigExpression () =
+    countArithmeticExpression bigExpression |> should equal 4059
+
+
+  let compositeExpression = Minus (bigExpression, expressionNumber)
+  [<Test>]
+  let checkCompositeExpression () =
+    countArithmeticExpression compositeExpression |> should equal 4054
