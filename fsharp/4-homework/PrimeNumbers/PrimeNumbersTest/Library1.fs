b60483d821c0df64f4066f38a3d87eb3cc7014ee
Konstantin Batoev <kbatoev@mail.ru>
2017-03-27 11:48:37 +0400

    Homework 4. Task 4

---
+++ fsharp/4-homework/PrimeNumbers/PrimeNumbersTest/Library1.fs
@@ -0,0 +1,32 @@
+﻿namespace PrimeNumbersTest
+
+open PrimeNumbers.Sequence
+open NUnit.Framework
+open FsUnit
+
+module Tests = 
+
+  [<Test>]
+  let check35ToNotBePrime () =
+    isPrime 35 |> should equal false
+
+  [<Test>]
+  let check101ToBePrime () =
+    isPrime 101 |> should equal true
+
+  let list = Seq.toList (Seq.take 100 (primeNumbersSeq ()))
+  [<Test>]
+  let check101ToBeInList () =
+    list |> should contain 101
+
+  [<Test>]
+  let check102ToNotBeInList () =
+    list |> should not' (contain 102)
+
+  [<Test>]
+  let checkAscendingOrder () =
+    list |> should be ascending
+
+  [<Test>]
+  let checkExistenceOf9239 () =
+    Seq.exists (fun x -> if x = 9239 then true else false) (primeNumbersSeq ()) |> should equal true
