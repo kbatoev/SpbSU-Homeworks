d26be39a99ff66ae7831aa1f71e1eb24ce6c9fcf
Konstantin Batoev <kbatoev@mail.ru>
2017-04-02 08:37:39 +0400

    Homework 5. Task 2

---
+++ fsharp/5-homework/PointFree/PointFree/Program.fs
@@ -0,0 +1,20 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.org
+// Дополнительную справку см. в проекте "Учебник по F#".
+
+namespace Functions
+
+module PointFreeFunctions = 
+
+    // let func x l = List.map (fun y -> y * x) l
+    // let func x = List.map (fun y -> y * x)
+    // let func x = List.map (fun y -> ((*) x) y)
+    // let func x = List.map ((*) x)
+    // let func x = ((*) >> List.map) x
+    let func = (*) >> List.map 
+
+    [<EntryPoint>]
+    let main argv = 
+        let list = [1; 4; 5; 6;]
+        printfn "List before: %A\n" list
+        printfn "List after multiplying by 4: %A\n" <| func 4 list
+        0 // возвращение целочисленного кода выхода
---
+++ fsharp/5-homework/PointFree/PointFreeTest/Library1.fs
@@ -0,0 +1,25 @@
+﻿namespace PointFreeTest
+
+open Functions.PointFreeFunctions
+open NUnit.Framework
+open FsUnit
+
+
+module Tests =
+
+  [<Test>]
+  let checkEmptyList () =
+    func 3 [] |> should equal []
+
+  [<Test>]
+  let checkListWithOneElement () =
+    func 5 [1] |> should equal [5]
+
+  [<Test>]
+  let checkBigList () =
+    func 2 [11; 100; 34; 1; 77; 2000] |> should equal [22; 200; 68; 2; 154; 4000]
+
+
+  [<Test>]
+  let checkMultiplyingByZero () =
+    func 0 [11; 100; 34; 1; 77; 2000] |> should equal [0; 0; 0; 0; 0; 0]
