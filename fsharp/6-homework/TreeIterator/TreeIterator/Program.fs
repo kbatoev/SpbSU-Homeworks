7c99cb890b7cab551cd6f6e6afda84e580593869
Konstantin Batoev <kbatoev@mail.ru>
2017-04-14 06:54:11 +0400

    Dirty

--- fsharp/6-homework/TreeIterator/TreeIterator/Program.fs
+++ fsharp/6-homework/TreeIterator/TreeIterator/Program.fs
@@ -1,16 +1,19 @@
 ﻿// Дополнительные сведения о F# см. на http://fsharp.org
 // Дополнительную справку см. в проекте "Учебник по F#".
 
+open System
 open System.Collections
 open System.Collections.Generic
 
-type BinaryTree<'T>(v : 'T, ?l : BinaryTree<'T>, ?r : BinaryTree<'T>) =
-  let mutable value : 'T = v
+//type Node<'T> = 'T * Node<'T> * Node<'T>
+
+type BinaryTree<'T when 'T :> IComparable<'T> >(v : 'T, ?l : BinaryTree<'T>, ?r : BinaryTree<'T>) =
+  let mutable value : Option<'T> = Some v
   let mutable left : Option<BinaryTree<'T>> = l
   let mutable right : Option<BinaryTree<'T>> = r
 
   let rec helper (tree : BinaryTree<'T>) = 
-      printfn "%A" <| tree.GetValue()
+      printf "%A " <| tree.GetValue()
       match tree.GetLeft () with
       | None -> ()
       | Some l -> helper l
@@ -19,17 +22,27 @@ type BinaryTree<'T>(v : 'T, ?l : BinaryTree<'T>, ?r : BinaryTree<'T>) =
       | Some r -> helper r
 
   interface IEnumerator<'T> with
-    member this.Current = v :> obj
+    member this.Current = value :> obj
     member this.MoveNext () = false
     member this.Reset () = ()
     member this.Dispose () = ()
-    member this.Current = v
+    member this.Current = v 
   
   member this.GetValue () = value
   member this.GetLeft () = left
   member this.GetRight () = right
 
   member this.Print () = helper this
+  member this.Remove elem = 
+    match value with
+    | None -> ()
+    | (Some x) -> match x.CompareTo(elem) with
+                  | 0 -> match (left, right) with
+                         | (None, None) -> value <- None
+                         | (Some l, None) -> value <- l.GetValue ()
+                                             right <- l.GetRight ()
+                                             left <- l.GetLeft ()
+  //                          then 
     
   //override this.GetEnumerator () = null
 
