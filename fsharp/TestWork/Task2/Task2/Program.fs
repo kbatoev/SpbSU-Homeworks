77ca5edc5d144c32b971d08df4d82444267b9ba6
Konstantin Batoev <kbatoev@mail.ru>
2017-04-14 09:06:41 +0400

    Task 2

---
+++ fsharp/TestWork/Task2/Task2/Program.fs
@@ -0,0 +1,37 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.org
+// Дополнительную справку см. в проекте "Учебник по F#".
+
+namespace Figures
+
+module Square =
+
+  let printSquare n =
+    match n with
+    | 1 -> printfn "*\n"
+    | x when x <= 0 -> printfn "side must be more than 0\n"
+    | _ ->
+        let side = String.replicate n "*"
+        printfn "%s" side
+        let edgeSide = "*" + String.replicate (n - 2) " " + "*"
+        let total = n - 2
+        let rec printEdgeSides i =
+          if i = total
+          then ()
+          else printfn "%s" edgeSide
+               printEdgeSides (i + 1)
+        printEdgeSides 0
+        printfn "%s\n" side
+
+  [<EntryPoint>]
+  let main argv =
+    printfn "Square with side -8:"
+    printSquare (-8)
+    printfn "Square with side 1:"
+    printSquare 1
+    printfn "Square with side 2:"
+    printSquare 2
+    printfn "Square with side 4:"
+    printSquare 4
+    printfn "Square with side 10:"
+    printSquare 10
+    0 // возвращение целочисленного кода выхода
