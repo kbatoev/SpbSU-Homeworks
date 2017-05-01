5ebd1b177e44135eb3ecf02f31714f92539b1c94
Konstantin Batoev <kbatoev@mail.ru>
2017-05-01 18:41:40 +0400

    Homework 7. Task 2

---
+++ fsharp/7-homework/CalculateWorkflow/CalculateWorkflow/Program.fs
@@ -0,0 +1,72 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.org
+// Дополнительную справку см. в проекте "Учебник по F#".
+
+namespace Workflows
+
+open Microsoft.FSharp.Core
+
+module Calculator =
+  let compareDoubles n1 n2 =
+    match (n1, n2) with
+    | (None, _) -> false
+    | (_, None) -> false
+    | (Some x1, Some x2) -> abs(x1 - x2) <= 0.000001
+
+  type NumberBuilder() =
+    member this.Bind(x, f) =
+      let rec convertListOfCharsToDouble list acc degree = 
+        match list with
+        | [] when degree <> 0.1 -> Some acc
+        | (x :: xs) when (int x) >= 48 && (int x) <= 57 ->
+          let digit = double <| int x - 48
+          if degree = 1.0
+          then convertListOfCharsToDouble xs (acc * 10.0 + digit) 1.0
+          else convertListOfCharsToDouble xs (acc + digit * degree) (degree / 10.0)
+        | x :: xs when (x = '.' || x = ',') && degree = 1.0 -> convertListOfCharsToDouble xs acc 0.1
+        | _ -> None
+
+
+      match (box x) with
+      | y when (y :? int) -> let number = double <| (y :?> int)
+                             f number
+      | y when (y :? double) -> let number = y :?> double
+                                f number
+      | y when (y :? string) -> let str = y :?> string
+                                let number = convertListOfCharsToDouble (Seq.toList str) 0.0 1.0
+                                match number with
+                                | None -> None
+                                | Some n -> f n
+      | _ -> None
+
+    member this.Return(x) =
+      Some x
+
+
+  [<EntryPoint>]
+  let main argv = 
+      let calculate = new NumberBuilder()
+
+      let res = calculate {
+          let! x = 123
+          let! y = "901"
+          let z = x + y
+          return z
+      }
+
+      let res2 = calculate {
+          let! a = "90.Hello, world!"
+          let! b = "38"
+          return a + b
+          }
+
+      let res3 = calculate {
+          let! a = "12.0"
+          let! b = "555,9"
+          return a + b
+          }
+
+      printfn "res = %A" <| res
+      printfn "res2 = %A" <| res2
+      printfn "res3 = %A" <| res3
+
+      0 // возвращение целочисленного кода выхода
---
+++ fsharp/7-homework/CalculateWorkflow/CalculateWorkflowTests/Library1.fs
@@ -0,0 +1,79 @@
+﻿namespace CalculateWorkflowTests
+
+open NUnit.Framework
+open FsUnit
+open Workflows
+
+
+module Tests =
+
+  let calculate = new Calculator.NumberBuilder()
+  
+  let simpleNumber = calculate {
+    let! x = "1"
+    return x
+    }
+
+  [<Test>]
+  let simpleTest() =
+    Calculator.compareDoubles simpleNumber (Some 1.0) |> should equal true
+
+  let sum = calculate {
+    let! a = "10"
+    let! b = "15.5"
+    return a + b
+    }
+  [<Test>]
+  let sumEquals25 () =
+    Calculator.compareDoubles sum (Some 25.5) |> should equal true
+  
+  
+  let sum2 = calculate {
+    let! a = "10000"
+    let! b = "25."
+    return a + b
+    }
+  [<Test>]
+  let pointInTheEnd () =
+    sum2 |> should equal None
+
+  
+  let letterInString = calculate {
+    let! x = "7.8"
+    let! y = "1239"
+    let! z = "96A34"
+    return x + y + z
+    }
+  [<Test>]
+  let checkLetterInString () =
+    letterInString |> should equal None
+
+  let workFlowWithIntNumber = calculate {
+    let! x = 90
+    let! y = "9.9"
+    return x + y
+    }
+  [<Test>]
+  let checkWorkFlowWithIntNumber () =
+    Calculator.compareDoubles workFlowWithIntNumber (Some 99.9) |> should equal true
+
+  let workFlowWithDoubleNumber = calculate {
+    let! x = 50.5
+    let! y = "5.0"
+    return x / y
+    }
+  [<Test>]
+  let checkWorkFlowWithDoubleNumber () =
+    Calculator.compareDoubles workFlowWithDoubleNumber (Some 10.1) |> should equal true
+
+  let workflowWithCommas = calculate {
+    let! x = "0,44"
+    let! y = "10,7"
+    return x * y
+    }
+  [<Test>]
+  let checkWorkflowWithCommas () =
+    Calculator.compareDoubles workflowWithCommas (Some 4.708) |> should equal true
+
+
+  
