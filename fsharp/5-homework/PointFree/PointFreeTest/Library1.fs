de8d203b2d2d997776862f352aff4f72a8f07d09
Konstantin Batoev <kbatoev@mail.ru>
2017-05-02 10:30:39 +0400

    Added FsCheck. Removed excess file

--- fsharp/5-homework/PointFree/PointFreeTest/Library1.fs
+++ fsharp/5-homework/PointFree/PointFreeTest/Library1.fs
@@ -3,6 +3,7 @@
 open Functions.PointFreeFunctions
 open NUnit.Framework
 open FsUnit
+open FsCheck
 
 
 module Tests =
@@ -18,8 +19,8 @@ module Tests =
   [<Test>]
   let checkBigList () =
     func 2 [11; 100; 34; 1; 77; 2000] |> should equal [22; 200; 68; 2; 154; 4000]
-
-
+  
+  let multiplyOnZero (xs : int list) = func 0 xs = List.init (List.length xs) (fun x -> 0) 
   [<Test>]
-  let checkMultiplyingByZero () =
-    func 0 [11; 100; 34; 1; 77; 2000] |> should equal [0; 0; 0; 0; 0; 0]
+  let checkMultiplyingOnZero () =
+    Check.Quick multiplyOnZero
