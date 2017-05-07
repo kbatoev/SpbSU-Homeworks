f21832f0946fe148b05565ce2fd222f23fc66c81
Konstantin Batoev <kbatoev@mail.ru>
2017-05-07 16:28:32 +0400

    Added tests

--- fsharp/8-homework/WebPages/WebPages/Program.fs
+++ fsharp/8-homework/WebPages/WebPages/Program.fs
@@ -1,52 +1,55 @@
 ﻿// Дополнительные сведения о F# см. на http://fsharp.org
 // Дополнительную справку см. в проекте "Учебник по F#".
+namespace WebPages
 
 open System.Net
 open Microsoft.FSharp.Control.WebExtensions
 open System.Text.RegularExpressions
 
-let fetchAsyncThatDownladsWebPageAndPrintsInfo(url:string) =
-    async {
-        try
-            let uri = new System.Uri(url)
-            let webClient = new WebClient()
-            let! html = webClient.AsyncDownloadString(uri)
-            printfn "%A" <| url + " -- " + string html.Length
-            return html
-        with
-            | ex -> printfn "%s" (ex.Message)
-                    return ex.Message
-    }
+module ExploreReferencesFromGivenURL =
+
+  let fetchAsyncThatDownladsWebPageAndPrintsInfo(url:string) =
+      async {
+          try
+              let uri = new System.Uri(url)
+              let webClient = new WebClient()
+              let! html = webClient.AsyncDownloadString(uri)
+              printfn "%A" <| url + " -- " + string html.Length
+              return html
+          with
+              | ex -> printfn "%s" (ex.Message)
+                      return ex.Message
+      }
 
 
-// this is pattern that finds references of type: <a href=http://some-Site.someDomain
-// (anySymbolsBeforeFirstQuote ") andThenQuote "
-// (anySymbolsBeforeFirstClosingBracket >) andThenClosingBracket >
-let regExprForReference = @"<a\shref=\x22https?://\w[\w-]*\.[a-zA-Z]+[^\x22]*\x22[^>]*>"
-let regex = new Regex(regExprForReference)
+  // this is pattern that finds references of type: <a href=http://some-Site.someDomain
+  // (anySymbolsBeforeFirstQuote ") andThenQuote "
+  // (anySymbolsBeforeFirstClosingBracket >) andThenClosingBracket >
+  let regExprForReference = @"<a\shref=\x22https?://\w[\w-]*\.[a-zA-Z]+[^\x22]*\x22[^>]*>"
+  let regex = new Regex(regExprForReference)
 
-let getReferencesFromPage url =
-  printfn "Page is %A" <| url
-  let contentOfUrl = fetchAsyncThatDownladsWebPageAndPrintsInfo url |> Async.RunSynchronously
+  let getReferencesFromPage url =
+    printfn "Page is %A" <| url
+    let contentOfUrl = fetchAsyncThatDownladsWebPageAndPrintsInfo url |> Async.RunSynchronously
 
-  let referencesTagCollection = regex.Matches contentOfUrl
-  let siteRegExpr = @"https?://\w[\w-]*\.[a-zA-Z]+[^\x22]*"
-  let siteRegex = new Regex(siteRegExpr)
+    let referencesTagCollection = regex.Matches contentOfUrl
+    let siteRegExpr = @"https?://\w[\w-]*\.[a-zA-Z]+[^\x22]*"
+    let siteRegex = new Regex(siteRegExpr)
 
-  printfn "There are %A references" <| referencesTagCollection.Count
-  let rec parseTagReferences number acc =
-    if number < referencesTagCollection.Count
-    then let reference = (referencesTagCollection.Item number).Value
-         let site = siteRegex.Match(reference).Value
-         parseTagReferences (number + 1) (site :: acc)
-    else acc
-  parseTagReferences 0 []
+    printfn "There are %A references" <| referencesTagCollection.Count
+    let rec parseTagReferences number acc =
+      if number < referencesTagCollection.Count
+      then let reference = (referencesTagCollection.Item number).Value
+           let site = siteRegex.Match(reference).Value
+           parseTagReferences (number + 1) (site :: acc)
+      else acc
+    parseTagReferences 0 []
 
-[<EntryPoint>]
-let main argv = 
-    let url = "http://hwproj.me/courses/20"
-    //let url = System.Console.ReadLine()
+  [<EntryPoint>]
+  let main argv = 
+      let url = "http://hwproj.me/courses/20"
+      //let url = System.Console.ReadLine()
 
-    let list = getReferencesFromPage url
-    list |> List.map fetchAsyncThatDownladsWebPageAndPrintsInfo |> Async.Parallel |> Async.RunSynchronously |> ignore
-    0 // возвращение целочисленного кода выхода
+      let list = getReferencesFromPage url
+      list |> List.map fetchAsyncThatDownladsWebPageAndPrintsInfo |> Async.Parallel |> Async.RunSynchronously |> ignore
+      0 // возвращение целочисленного кода выхода
