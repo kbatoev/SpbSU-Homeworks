c29ea016506ca7785b69515f4c5d0580847a1cc0
Konstantin Batoev <kbatoev@mail.ru>
2017-05-07 15:35:32 +0400

    Homework 8. Task 1

---
+++ fsharp/8-homework/WebPages/WebPages/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace WebPages.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// Общие сведения о сборке можно задать с помощью следующего 
+// набора атрибутов. Отредактируйте эти значения атрибутов, чтобы изменить сведения,
+// связанные с этой сборкой.
+[<assembly: AssemblyTitle("WebPages")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("WebPages")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// При установке значения False в атрибуте ComVisible типы в этой сборке становятся невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте для атрибута ComVisible значение True для этого типа.
+[<assembly: ComVisible(false)>]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект видим для COM
+[<assembly: Guid("4c9e8157-ff51-4359-96d2-a9db0cbc0df4")>]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+// 
+//       Основной номер версии
+//       Дополнительный номер версии 
+//       Номер сборки
+//      Редакция
+// 
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [<сборка: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ fsharp/8-homework/WebPages/WebPages/Program.fs
@@ -0,0 +1,61 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.org
+// Дополнительную справку см. в проекте "Учебник по F#".
+
+open System.Net
+open Microsoft.FSharp.Control.WebExtensions
+open System.Text.RegularExpressions
+
+let fetchAsyncThatDownladsWebPage(url:string) =
+    async {
+        try
+            let uri = new System.Uri(url)
+            let webClient = new WebClient()
+            return! webClient.AsyncDownloadString(uri)
+        with
+            | ex -> printfn "%s" (ex.Message);
+                    return ex.Message
+    }
+
+let fetchAsyncThatDownladsWebPageAndPrintsInfo(url:string) =
+    async {
+        try
+            let uri = new System.Uri(url)
+            let webClient = new WebClient()
+            let! html = webClient.AsyncDownloadString(uri)
+            printfn "%A" <| url + " -- " + string html.Length
+        with
+            | ex -> printfn "%s" (ex.Message);
+    }
+
+
+// this is pattern that finds references of type: <a href=http://someSite.someDomain
+// anySymbolBeforeFirstQuote " andThenQuote "
+// anySymbolBeforeFirstClosingBracket > andThenClosingBracket >
+let regExprForReference = @"<a\shref=\x22https?://\w+\.[a-zA-Z][^\x22]*\x22[^>]*>"
+let regex = new Regex(regExprForReference)
+
+let getReferencesFromPage url =
+  printfn "Page is %A" <| url
+  let contentOfUrl = fetchAsyncThatDownladsWebPage url |> Async.RunSynchronously
+
+  let referencesTagCollection = regex.Matches contentOfUrl
+  let siteRegExpr = @"https?://\w+\.\w[^\x22]*"
+  let siteRegex = new Regex(siteRegExpr)
+
+  printfn "There are %A references" <| referencesTagCollection.Count
+  let rec parseTagReferences number acc =
+    if number < referencesTagCollection.Count
+    then let reference = (referencesTagCollection.Item number).Value
+         let site = siteRegex.Match(reference).Value
+         parseTagReferences (number + 1) (site :: acc)
+    else acc
+  parseTagReferences 0 []
+
+[<EntryPoint>]
+let main argv = 
+    let url = "http://hwproj.me/courses/20"
+    //let url = System.Console.ReadLine()
+
+    let list = getReferencesFromPage url
+    list |> List.map fetchAsyncThatDownladsWebPageAndPrintsInfo |> Async.Parallel |> Async.RunSynchronously |> ignore
+    0 // возвращение целочисленного кода выхода
